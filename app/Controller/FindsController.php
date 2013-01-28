974ed41d177b074c6b0753b65aef68d6d2ea470f
Digithis <rolletremi@gmail.com>
2013-01-28 17:35:05 +0100

    FindController Skeleton

---
+++ app/Controller/FindsController.php
@@ -0,0 +1,23 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+* @author 
+*/
+
+
+class FindsController extends AppController{
+	# j'aime les carottes
+
+	// headless
+	public $uses = array();
+	
+	
+	public function find($value=''){
+		# code...
+		///$this->loadModel();
+		
+	}
+
+}
