ebfcb566fc5c1701b85c6ed8377d5066912ff763
Digithis <rolletremi@gmail.com>
2013-01-29 01:13:25 +0100

    FindsController.php
    Getting late, need some tea

--- app/Controller/FindsController.php
+++ app/Controller/FindsController.php
@@ -6,19 +6,56 @@ App::uses('AppController', 'Controller');
 */
 
 class FindsController extends AppController{
-
-	// headless
+	public $tables = array(
+		'Users'=>array('username','email'),
+		'Guides'=>array('slogan')
+	);
 	public $uses = array();
 
 /**
 *  Fonction de recherche find()
-**/
-	public function find(){
-		$entry = $this->data;
-		$entry = strtolower(current($entry));
-		debug($entry);	
-
+**/		
+		
+		public function find(){
+		foreach($this->tables as $k=>$v){
+			array_push($this->uses,$k);
+		}
+		if(empty($this->data)){
+			
+		}
+		else{
+			$results = array();
+			$entry = $this->data;
+			$entry = strtolower(current($entry['Find']));
+			
+			foreach($this->tables as $k=>$v):
+				
+			$results=$this->getResults($k,$entry,$results,$v);
+			
+			endforeach;
+			debug($results);		
+		}
 	}
+	
+	function getResults($table,$entry,$results,$columns){
+		foreach($columns as $v){
+			$results[$table][$v]=array();
+			$data = $this->$table->find('all', array( 
+				'conditions' => array($table.'.'.$v.' LIKE' => '%'.$entry.'%') 
+			));
+	
+			if(!empty($data)){
+				foreach($data as $v2){
+					array_push($results[$table][$v],current($v2));
+				}
+			}
+			else{
+				$results[$table][$v] = "Pas de résultat dans cette column";
+			}
+		}
 
+		return $results;
+	}
+		
 
 }
--- app/View/Finds/find.ctp
+++ app/View/Finds/find.ctp
@@ -1,6 +1,9 @@
-<?php echo $this->Form->create(array(
-	'action'=>'find'
-	)); ?>
+<?php
+if(isset($message)){
+	echo $message;
+}
+?>
+<?php echo $this->Form->create('Find') ?>
 
 <?php echo $this->Form->input("search_text",array(
 	'label'=>'Rechercher'
