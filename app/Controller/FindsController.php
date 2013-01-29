754985761b195d72ecb014485d10a8d0ea62345a
Digithis <rolletremi@gmail.com>
2013-01-30 00:25:38 +0100

    FindsController.php, finds/index.ctp
    crabs & cigars

--- app/Controller/FindsController.php
+++ app/Controller/FindsController.php
@@ -6,14 +6,23 @@ App::uses('AppController', 'Controller');
 */
 
 class FindsController extends AppController{
+	public $theme = "Bootstrap";
 	
 	public $tables = array(
-		'Users'	  => array('username','email'),
-		'Journeys'  =>array('about','name','body'),
-		'Tourists'=>array('bio','first_name','last_name')
+		'Tourists'=>array(
+			'infos'=>array('bio','first_name','last_name'),
+			'displayfield' =>'first_name'
+		),
+		'Users'=>array(
+			'infos'=>array('username','email'),
+			'displayfield' =>'username'
+		),
+		'Journeys'=>array(
+			'infos'=>array('name','about','body'),
+			'displayfield' =>'name'
+		)
 	);
 	public $uses = array();
-	
 	public $total = 0;
 
 /**
@@ -35,13 +44,13 @@ class FindsController extends AppController{
 		$entry = strtolower(current($entry['Find']));
 	
 		foreach($this->tables as $k=>$v):
-			$results=$this->getResults($k,$entry,$results,$v);	
+				$results=$this->getResults($k,$entry,$results,$v['infos'],$v['displayfield']);	
 		endforeach;
 
 		foreach ($results as $v):
 		    $this->total+= count($v);
 		endforeach;
-		
+
 		$d['total']   = $this->total;
 		$d['results'] = $results;
 		$this->set($d);		
@@ -51,7 +60,7 @@ class FindsController extends AppController{
 /**
 *  Fonction de comparaison des entrées dans les tables indiquées => getResults()
 **/
-	function getResults($table,$entry,$results,$columns){
+	function getResults($table,$entry,$results,$columns,$displayfield){
 	foreach($columns as $v){
 		$data = $this->$table->find('all', array( 
 			'conditions' => array($table.'.'.$v.' LIKE' => '%'.$entry.'%') 
@@ -61,7 +70,9 @@ class FindsController extends AppController{
 			$results[$table]=array();
 			
 			foreach($data as $v2):
-				array_push($results[$table],current($v2));	
+				$v2=current($v2);
+				$v2['displayfield']=$displayfield;
+				array_push($results[$table],$v2);
 			endforeach;
 		}
 	}
--- app/View/Finds/index.ctp
+++ app/View/Finds/index.ctp
@@ -1,27 +1,51 @@
+
 <?php
 echo $this->Form->create('Find');
 echo $this->Form->input("search_text",array(
-	'label'=>'Rechercher'
+	'label'=>'Rechercher sur le site'
 ));
 echo $this->Form->end("Rechercher");
-
 if(isset($results)){
 	
 	if($total!=0){
 		echo 'Il y\'a '.$total.' résultats correspondant à votre recherche';
-		debug($results);
+		$user_ids=array();
+		$hide_data=false;
+		
 		foreach($results as $type=>$type_result){
 			echo '<h3>';
 			echo $type;
 			echo '</h3>';
-			echo '<ul>';
+			echo '<ul class="nav nav-tabs nav-stacked">';
+			
 			foreach($type_result as $fields){
 				
-				foreach($fields as $field_name => $field_values){
-					
-					echo '<li>'.$field_values.'</li>';
-					
+				if($type=="Tourists"){
+					array_push($user_ids,$fields['user_id']);
 				}
+				
+				else if($type=="Users"){
+					foreach($user_ids as $v){
+						if($fields['id']==$v){
+							$hide_data = true;
+						}
+					}
+				}
+				
+				else if($type=="Journeys"){
+					if(!$fields['public']){
+						$hide_data = true;
+						echo '<li><a style="font-size:13px" class="alert alert-error">Journey privée</a></li>';
+					}
+				}
+				
+				if(!$hide_data){
+					echo '<li>';
+					echo $this->Html->link($fields[$fields['displayfield']], '/'.strtolower($type).'/view/'.$fields['id'], array('class' => 'button', 'target' => '_blank'));
+					echo '</li>';
+				}
+				
+				$hide_data=false;
 			}
 			echo '</ul>';
 		}
