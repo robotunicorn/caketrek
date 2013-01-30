670d2bf30f3178d88f441f20b99d6ad0eb1b6e44
Digithis <rolletremi@gmail.com>
2013-01-31 00:44:07 +0100

    ajax ftw

--- app/Controller/FindsController.php
+++ app/Controller/FindsController.php
@@ -11,15 +11,18 @@ class FindsController extends AppController{
 	public $tables = array(
 		'Tourists'=>array(
 			'infos'=>array('bio','first_name','last_name'),
-			'displayfield' =>'first_name'
+			'displayfields' =>array('first_name')
 		),
 		'Users'=>array(
 			'infos'=>array('username','email'),
-			'displayfield' =>'username'
-		),
+			'displayfields' => array('username'),
+			'show' => array(
+				'name'=>'Tourists',
+				'key'=>'user_id'
+		)),
 		'Journeys'=>array(
 			'infos'=>array('name','about','body'),
-			'displayfield' =>'name'
+			'displayfields' =>array('name','public')
 		)
 	);
 	public $uses = array();
@@ -29,54 +32,108 @@ class FindsController extends AppController{
 * 	Fonction index() => Traitement des données envoyées par le formulaire
 **/	
 	public function index(){
-
-	foreach($this->tables as $k=>$v):
-		array_push($this->uses,$k);
-	endforeach;
-	
-	if(empty($this->data)){
-		//todo
+		
 	}
-	else{
-		$results = array();
-			
-		$entry = $this->data;
-		$entry = strtolower(current($entry['Find']));
-	
-		foreach($this->tables as $k=>$v):
-				$results=$this->getResults($k,$entry,$results,$v['infos'],$v['displayfield']);	
-		endforeach;
+	function ajaxProcess(){
+	// Cas des requêtes AJAX
+	        if ( $this->request->is( 'ajax' ) ){
+				foreach($this->tables as $k=>$v):
+					array_push($this->uses,$k);
+				endforeach;
+				$results = array();
 
-		foreach ($results as $v):
-		    $this->total+= count($v);
-		endforeach;
+				$entry = $this->request->query[ 'entry' ];
+				$entry = strtolower($entry);
 
-		$d['total']   = $this->total;
-		$d['results'] = $results;
-		$this->set($d);		
-		}
-	}
+				foreach($this->tables as $k=>$v):
+							$results=$this->getResults($k,$entry,$results,$v['infos'],$v['displayfields']);	
+				endforeach;
+
+				//$this->set($d);
+
+	            // On encode au format JSON et on affiche directement ce résultat (pour le récupérer dans la vue)
+	            echo json_encode($results);
+
+	            // Il faut penser à terminer le script brutalement pour court-circuiter les mécanismes
+	            // de CakePHP (méthodes de la classe mère AppController par exemple)
+	            exit();
+	        }
+	        else {
+	            // Code qui servirait dans le cas de requêtes http classiques (par opposition à AJAX)
+	            // Pour nous dans cet exemple, c'est inutile...
+	        }
+	    }
 
 /**
 *  Fonction de comparaison des entrées dans les tables indiquées => getResults()
 **/
-	function getResults($table,$entry,$results,$columns,$displayfield){
-	foreach($columns as $v){
-		$data = $this->$table->find('all', array( 
-			'conditions' => array($table.'.'.$v.' LIKE' => '%'.$entry.'%') 
-		));
-	
-		if(!empty($data)){
-			$results[$table]=array();
-			
-			foreach($data as $v2):
-				$v2=current($v2);
-				$v2['displayfield']=$displayfield;
-				array_push($results[$table],$v2);
-			endforeach;
+	function getResults($table,$entry,$results,$columns,$displayfields){
+		$conditions = array();
+		foreach($columns as $v){
+		    $conditions[] = array($table.'.'.$v.' LIKE' => '%'.$entry.'%');
 		}
-	}
+		// On recherche les correspondances dans la BDD
+		$data = $this->$table->find('all', array( 
+			'conditions' => array('OR' => $conditions)) 
+		);
+		// Si il y'a correspondance
+		if(!empty($data)){
+			//Si le type traité doit afficher un autre type (par exemple Users->show->Tourists)
+			if(isset($this->tables[$table]['show'])){
+				
+				$show_name = $this->tables[$table]['show']['name'];
+				$show_key  = $this->tables[$table]['show']['key'];
+				$displayfields = $this->tables[$show_name]['displayfields'];
+				
+				foreach($data as $search_results): //On traites les données de la recherche dans un foreach
+					$itexists = false;
+					$search_results = current($search_results);
+					$id = $search_results['id'];
+					 // ID du résultat
+					$show_data = $this->$show_name->find('all', array(
+						'conditions' => array($show_name.'.'.$show_key => $id)
+					));
+					
+					if(!empty($show_data)){ //S'il y'a correspondance entre les deux types (par exemple si Users est lié à Tourists)
+						
+					$show_data = current(current($show_data));
+						if(!isset($results[$show_name])){ 
+							$results[$show_name] = array();
+						}
+						foreach($results[$show_name] as $valeur){
+							if($valeur['id']==$id){
+								$itexists = true;
+							}
+						}	
+						if(!$itexists){
+							$show_data['displayfields']=$displayfields;
+							$show_data['normal']='lol';
+							array_push($results[$show_name],$show_data); // On ajoute les valeurs à notre array results		
+						}
+					}
+				endforeach;						
+			}
+			else{
+				if(!isset($results[$table])){
+					$results[$table]=array();
+				}
+				foreach($data as $search_results):
+					$itexists = false;
+					$search_results=current($search_results);
+					
+						foreach($results[$table] as $valeur){
+							if($valeur['id']==$search_results['id']){
+								$itexists = true;
+							}
+						}
+						if(!$itexists){
+							$search_results['displayfields']=$displayfields;
+							$search_results['normal']='normal';
+							array_push($results[$table],$search_results);// On ajoute les valeurs à notre array results
+						}
+				endforeach;
+			}
+		}	
 	return $results; //return array de résultats
-	}
-
+	}	
 }
--- app/View/Finds/index.ctp
+++ app/View/Finds/index.ctp
@@ -1,54 +1,37 @@
+<script type="text/javascript" charset="utf-8">
+$(document).ready(function(){
+	$('.search-input').keyup(function(){
+		$('.results').html('');
+		//var siteurl = 'lol';
+		var searchtext = $(this).val();
+		$.get("finds/ajaxProcess?entry="+searchtext,function(data) {
+		var data = jQuery.parseJSON(data);
+		 $.each(data, function(key,value) {
+			$('.results').append('<ul id="ul-'+key.toLowerCase()+'" class="nav nav-tabs nav-stacked"><li style="margin-top:15px; margin-bottom:5px;"><span style="font-size:13px" class="label label-info">'+key+'</span></li></ul>');
+	       	$.each(value, function(k,v){
+				var displayfield = v.displayfields[0];
+				$('#ul-'+key.toLowerCase()).append('<li><a href="'+key.toLowerCase()+'/view/'+v.id+'" target="_blank">'+v[displayfield]+'</a></li>');
+			});
+		
+	      });
+
+		});
+	});
+});
+</script>
 <?php
 echo $this->Form->create('Find');
 echo $this->Form->input("search_text",array(
-	'label'=>'Rechercher sur le site'
+	'label'=>'Rechercher sur le site',
+	'id'=>'search-'.rand(),
+	'name'=>'search-'.rand(),
+	'class'=>'search-input'
 ));
-echo $this->Form->end("Rechercher");
-if(isset($results)){
-	
-	if($total!=0){
-		echo '<span style="font-size:13px" class="label label-success">Il y\'a '.$total.' résultats correspondant à votre recherche</span>';
-		$user_ids=array();
-		$hide_data=false;
-		echo '<ul class="nav nav-tabs nav-stacked">';
-		foreach($results as $type=>$type_result){
-
-			$loops=0;
-			foreach($type_result as $fields){
-				
-				if($type=="Tourists"){
-					array_push($user_ids,$fields['user_id']);
-				}
-				
-				else if($type=="Users"){
-					foreach($user_ids as $v){
-						if($fields['id']==$v){
-							$hide_data = true;
-						}
-					}
-				}	
-				if(!$hide_data){
-					
-					if($loops<1){
-						echo '<li style="margin-top:15px; margin-bottom:5px;"><span style="font-size:13px" class="label label-info">'.$type.'</span></li>';
-						$loops++;
-					}
-					if($type == "Journeys" AND !$fields['public']){
-						echo '<li><a style="font-size:13px" class="alert alert-error">Journey privée</a></li>';
-					}
-					else{
-						echo '<li>';
-						echo $this->Html->link($fields[$fields['displayfield']], '/'.strtolower($type).'/view/'.$fields['id'], array('class' => 'button', 'target' => '_blank'));
-						echo '</li>';
-					}
-				}
-				$hide_data=false;
-			}
-		}
-	echo '</ul>';
-	}
-	else{
-		echo '<span style="font-size:13px" class="label label-defaut">Il n\'y a aucun résultat correspondant à votre recherche</span>';
-	}
-}
 ?>
+<a id="lol" href="#">Ajax</a>
+<?php
+echo $this->Form->end("Rechercher");
+?>
+<div class="results" style="width:80%">
+
+</div>
