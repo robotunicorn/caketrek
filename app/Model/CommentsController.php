02f5e36dd66b81e27b31df33c619e578d42d1202
Fabien <Rutteau>
2013-01-30 15:39:43 +0100

    Comment

---
+++ app/Model/CommentsController.php
@@ -0,0 +1,36 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+* Comments Controller
+*
+* @property Comment $Comment
+*/
+class CommentsController extends AppController {
+
+	public function index() {
+
+	}
+/**
+* add method
+*
+* @return void
+*/
+	public function add() {
+
+		if ($this->request->is('post')) {
+
+			$this->Comment->create();
+
+			if ($this->Comment->save($this->request->data)) {
+				$this->Session->setFlash(__('The comment has been saved'));
+				$this->redirect(array('action' => 'index'));
+
+			} else {
+
+				$this->Session->setFlash(__('The comment could not be saved. Please, try again.'));
+				
+			}
+			die();
+		}
+	}
+}
